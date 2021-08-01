d8bd6d8c3cdb152266ed5c417a6f3b12a1e267ec
sandvik16 <sannndvik16@gmail.com>
2021-08-01 21:47:24 +0300

    style for onOff component

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import './App.css';
 import {Accordion} from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import {OnOff} from "./components/OnOff/OnOff";
 
 
 export const App = (props: any) => {
     console.log('App rendering')
     return (
         <div>
+            <OnOff on={true}/>
+            {/*<Accordion titleValue={'Menu'} collapsed={true}/>*/}
+            {/*<Accordion titleValue={'Users'} collapsed={false}/>*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
 
-            <Accordion titleValue={'Menu'} collapsed={true}/>
-            <Accordion titleValue={'Users'} collapsed={false}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
         </div>
 
     );
--- src/components/OnOff/OnOff.tsx
+++ src/components/OnOff/OnOff.tsx
@@ -1,12 +1,48 @@
 import React from "react";
+import s from './OnOff.module.css'
 
-export const OnOff = () => {
+type PropsType = {
+    on: boolean
+}
 
-    return(
+
+export const OnOff = (props: PropsType) => {
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.on ? 'green' : 'white'
+    };
+
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '2px',
+        padding: '2px',
+        backgroundColor: props.on ? 'white' : 'red'
+    };
+
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '5px',
+        border: '1px solid black',
+        display: 'inline-block',
+        marginLeft: '5px',
+        backgroundColor: props.on ? 'green' : 'red'
+
+    }
+
+    return (
         <div>
-            <div className={s.on}>On</div>
-            <div className={s.off}>Off</div>
-            <div className={s.indicator}></div>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
         </div>
     )
 }
