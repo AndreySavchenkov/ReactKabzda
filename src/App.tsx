13da85568fab82fd7bcb7fc66d4d5facf6e09561
sandvik16 <sannndvik16@gmail.com>
2021-08-01 17:47:31 +0300

    added onoff component

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,12 @@ export const App = (props: any) => {
 
             <Accordion titleValue={'Menu'} collapsed={true}/>
             <Accordion titleValue={'Users'} collapsed={false}/>
-
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
 
     );
---
+++ src/components/OnOff/OnOff.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const OnOff = () => {
+
+    return(
+        <div>
+            <div className={s.on}>On</div>
+            <div className={s.off}>Off</div>
+            <div className={s.indicator}></div>
+        </div>
+    )
+}
+
