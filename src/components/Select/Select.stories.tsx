52137434be9b1bae1b3fdbb32a0caaca585a2275
sandvik16 <sannndvik16@gmail.com>
2021-08-20 08:14:33 +0300

    controlled select

---
+++ src/components/Select/Select.stories.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {ComponentStory, ComponentMeta, Story} from '@storybook/react';
+import {Select, SelectPropsType} from "./Select";
+import {action} from "@storybook/addon-actions";
+
+export default {
+    title: 'Select',
+    component: Select,
+}
+
+export const WithValue: Story<SelectPropsType> = (args) =>
+    <>
+        <Select
+            onChange={action('value changed')}
+            value={'2'}
+            items={[
+                {value: '1', title: 'Minsk'},
+                {value: '2', title: 'Grodno'},
+                {value: '3', title: 'Lodz'}
+            ]}
+        />
+    </>
+
+export const WithoutValue: Story<SelectPropsType> = (args) =>
+    <>
+        <Select
+            onChange={action('value changed')}
+            items={[
+                {value: '1', title: 'Minsk'},
+                {value: '2', title: 'Grodno'},
+                {value: '1', title: 'Lodz'}
+            ]}
+        />
+    </>
+
+
+
